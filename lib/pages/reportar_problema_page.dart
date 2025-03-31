d4a381f0248a2366fa4a041bcad7b8ac9250ef63
LinikerThiers <linikerthiers@gmail.com>
2025-03-31 17:34:45 -0300

    mudando o tamanho do botao no relatar problema

--- lib/pages/reportar_problema_page.dart
+++ lib/pages/reportar_problema_page.dart
@@ -30,89 +30,92 @@ class _ReportarProblemaPageState extends State<ReportarProblemaPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            TextField(
-              controller: _problemController,
-              decoration: InputDecoration(
-                hintStyle: TextStyle(
-                    color: isDarkMode ? Colors.grey[300] : Colors.black),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                labelText: "DESCREVA_PROBLEMA".tr(),
-                labelStyle: TextStyle(
-                  color: isDarkMode ? Colors.grey[300] : Colors.black,
-                ),
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: isDarkMode ? Colors.white : Colors.black,
-                    width: 2.0,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: isDarkMode ? Colors.grey[300]! : Colors.black,
-                    width: 1,
-                  ),
-                ),
-                counterText: '${_problemController.text.length}/$_maxLength',
-                counterStyle: TextStyle(
-                  color: _problemController.text.length > _maxLength
-                      ? Colors.red
-                      : isDarkMode
-                          ? Colors.grey[300]
-                          : Colors.black,
-                ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-              style: TextStyle(
-                color: isDarkMode ? Colors.grey[300] : Colors.black,
-              ),
-              maxLines: 10,
-              minLines: 6,
-              keyboardType: TextInputType.multiline,
-              maxLength: _maxLength,
-              buildCounter: (context,
-                  {required currentLength, required isFocused, maxLength}) {
-                return Text(
-                  '$currentLength/$maxLength',
-                  style: TextStyle(
-                    color: currentLength > maxLength!
+              TextField(
+                controller: _problemController,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                      color: isDarkMode ? Colors.grey[300] : Colors.black),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  labelText: "DESCREVA_PROBLEMA".tr(),
+                  labelStyle: TextStyle(
+                    color: isDarkMode ? Colors.grey[300] : Colors.black,
+                  ),
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: isDarkMode ? Colors.white : Colors.black,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: isDarkMode ? Colors.grey[300]! : Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  counterText: '${_problemController.text.length}/$_maxLength',
+                  counterStyle: TextStyle(
+                    color: _problemController.text.length > _maxLength
                         ? Colors.red
                         : isDarkMode
                             ? Colors.grey[300]
                             : Colors.black,
                   ),
-                );
-              },
-              onChanged: (text) {
-                setState(() {});
-              },
-            ),
-            const SizedBox(height: 100),
-            TextButton(
-              onPressed: _problemController.text.trim().isEmpty ||
-                      _problemController.text.length > _maxLength
-                  ? null
-                  : () {
-                    //Lógica para enviar o problema reportado
-                  },
-              style: TextButton.styleFrom(
-                minimumSize: const Size(300, 50),
-                backgroundColor: isDarkMode ? Colors.white : Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                ),
+                style: TextStyle(
+                  color: isDarkMode ? Colors.grey[300] : Colors.black,
+                ),
+                maxLines: 10,
+                minLines: 10,
+                keyboardType: TextInputType.multiline,
+                maxLength: _maxLength,
+                buildCounter: (context,
+                    {required currentLength, required isFocused, maxLength}) {
+                  return Text(
+                    '$currentLength/$maxLength',
+                    style: TextStyle(
+                      color: currentLength > maxLength!
+                          ? Colors.red
+                          : isDarkMode
+                              ? Colors.grey[300]
+                              : Colors.black,
+                    ),
+                  );
+                },
+                onChanged: (text) {
+                  setState(() {});
+                },
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: _problemController.text.trim().isEmpty ||
+                        _problemController.text.length > _maxLength
+                    ? null
+                    : () {
+                        // Lógica para enviar o problema reportado
+                      },
+                style: TextButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  backgroundColor: isDarkMode ? Colors.white : Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'ENVIAR'.tr(),
+                  style: TextStyle(
+                      color: isDarkMode ? Colors.black : Colors.white),
                 ),
               ),
-              child: Text(
-                'ENVIAR'.tr(),
-                style:
-                    TextStyle(color: isDarkMode ? Colors.black : Colors.white),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
